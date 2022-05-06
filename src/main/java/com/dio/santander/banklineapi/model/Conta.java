183f9c73c30c6ec86e963b7645d44076da4d0b36
Rafael Toledo <toledorafael304@gmail.com>
2022-05-05 21:55:03 -0300

    Adicionando os arquivos do SpringBoot

---
+++ src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -0,0 +1,24 @@
+package com.dio.santander.banklineapi.model;
+
+public class Conta {
+
+    private long numero;
+    private Double saldo;
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+}
